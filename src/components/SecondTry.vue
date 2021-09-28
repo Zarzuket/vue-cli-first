611539fdafcddc8ab4dd2e8941f34e67bd56adb8
Zarzuket <davide.bajo@gmail.com>
2021-09-28 16:17:17 +0200

    add exercise

---
+++ src/components/SecondTry.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img src="https://cdn.wallpapersafari.com/87/14/XBJNwL.jpg" alt="Pa">
+    </div>
+</template>
+
+<script>
+export default {
+    name:"SecondTry"
+}
+</script>
+
+<style>
+div{
+    margin: auto;
+    height: 31.25rem;
+}
+img{
+    width: 90%;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: "Title",
+
+}
+</script>
+
+<style>
+h1{
+    height: 1.25rem;
+}
+</style>
